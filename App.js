97942a8edf89815cbb2910a1f4cb079c11d13e45
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-09-09 16:36:57 +0800

    Installed React Native UI Kitten

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Sample React Native App With React Native UI Kitten
  * https://github.com/facebook/react-native
  *
  * @format
@@ -8,6 +8,18 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {RkButton, RkCard, RkTheme} from 'react-native-ui-kitten';
+
+let accent = '#000000';
+
+RkTheme.setType('RkButton', 'accent', {
+  container: {
+    backgroundColor: accent
+  },
+  content: {
+    color: 'white'
+  }
+});
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,7 +36,18 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.instructions}>Edited By Jerry Chong</Text>
+        <RkButton rkType='accent'>Click me!</RkButton>
+        <RkCard>
+          <View rkCardHeader>
+            <Text>Header</Text>
+          </View>
+          <View rkCardContent>
+            <Text> quick brown fox jumps over the lazy dog</Text>
+          </View>
+          <View rkCardFooter>
+            <Text>Footer</Text>
+          </View>
+        </RkCard>
       </View>
     );
   }
